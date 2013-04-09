eccd5e40f552717b8654e9a219a67f576935b988
Ondrej Zara <ondrej.zara@gmail.com>
2013-04-09 16:06:34 +0200

    workers

--- js/operation-invert.js
+++ js/operation-invert.js
@@ -5,20 +5,22 @@ Operation.Invert.prototype = Object.create(Operation.prototype);
 
 Operation.Invert.prototype.go = function() {
 	var promise = Operation.prototype.go.call(this);
+	var ts = Date.now();
+
 	var w = this._source.width;
 	var h = this._source.height;
 	var id = this._source.getContext("2d").getImageData(0, 0, w, h);
-	for (var i=0;i<w*h;i++) {
-		id.data[4*i+0] = 255-id.data[4*i+0];
-		id.data[4*i+1] = 255-id.data[4*i+1];
-		id.data[4*i+2] = 255-id.data[4*i+2];
-	}
-
-	this._target = document.createElement("canvas");
-	this._target.width = w;
-	this._target.height = h;
-	this._target.getContext("2d").putImageData(id, 0, 0);
-	promise.fulfill(this);
+	var worker = new Worker("js/worker-invert.js");
+	worker.postMessage(id);
+	
+	worker.onmessage = function(e) {
+		this._target = document.createElement("canvas");
+		this._target.width = w;
+		this._target.height = h;
+		this._target.getContext("2d").putImageData(e.data, 0, 0);
+		console.log(Date.now()-ts);
+		promise.fulfill(this);
+	}.bind(this);
 
 	return promise;
 }
